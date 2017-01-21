898d64a14d754fd1e1a0ef04c8302959ecfe5e5e
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-01-21 14:55:40 -0800

    added some more features

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 let blue = false;
 let red = false;
 let selectedColor = undefined;
+let grid = document.querySelector('.grid');
+console.log(grid);
 
 
 //select all squares in grid
@@ -36,3 +38,45 @@ function selectColor(event) {
 palette.forEach(function(element){
   element.addEventListener('click', selectColor);
 })
+
+//Custom Color Selection
+let custom = document.querySelector('.custom');
+custom.addEventListener('click', customColor);
+
+function customColor() {
+  let colorChoice = prompt('Please enter a valid color name');
+  selectedColor = colorChoice;
+  custom.style.backgroundColor = colorChoice;
+
+}
+
+//Paintbrush Dragging Functionality
+let drag = false;
+canvas.forEach(function(element){
+  element.addEventListener('mousedown', startDrag);
+  element.addEventListener('mouseup', stopDrag);
+})
+
+
+function startDrag() {
+  drag = true;
+  console.log(drag);
+  event.target.style.backgroundColor = selectedColor;
+
+  grid.addEventListener('mouseover', tester);
+
+  }
+
+
+
+function stopDrag() {
+  drag = false;
+  console.log(drag);
+  grid.removeEventListener('mouseover', tester);
+
+}
+
+function tester() {
+  console.log('weeeee');
+  event.target.style.backgroundColor = selectedColor;
+}
